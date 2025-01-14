9fd7943926d4fffc13ef7227a76504e88f2ac2d9
Eunji Yoo <eunjiu32@gmail.com>
2025-01-15 04:21:33 +0900

    product card

---
+++ shoekingshop/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import ProductCard from './component/productCard'
+
+function App() {
+  return (
+    <>
+    <div className='productLayout'>
+      <ProductCard/>
+      <ProductCard/>
+    </div>
+
+
+    </>
+  )
+}
+
+export default App
